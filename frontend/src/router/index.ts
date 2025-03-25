016098c80a081a9f59eb2f0584868c09ff828a5f
Zangar <zanggar.muratkhan@moravio.com>
2025-03-25 11:24:43 +0500

    Added Home page

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,15 +1,23 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import HomePage from "@/views/HomePage.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      path: '/',
+      name: 'home',
+      component: HomePage,
+    },
+    {
       path: '/auth/sign-in',
       name: 'sign-in',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/auth/SignIn.vue'),
+      component: () => import('../views/auth/SignInPage.vue'),
+    },
+    {
+      path: '/transactions',
+      name: 'transactions',
+      component: () => import('../views/transaction/TransactionsPage.vue'),
     },
   ],
 })
---
+++ frontend/src/views/HomePage.vue
@@ -0,0 +1,64 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <main class="w-auto">
+    <h1 class="h3 mb-3 fw-normal">Добро пожаловать, Имя!</h1>
+    <p class="mb-4">Ваш текущий баланс: $500.25</p>
+    <div class="list-group">
+      <div class="list-group-item list-group-item-action py-3">
+        <div class="d-flex gap-2 w-100 justify-content-between">
+          <div>
+            <h6 class="mb-0">Пополнение баланса</h6>
+            <p class="mb-2 opacity-75">2025-03-17 13:18:55</p>
+            <p class="mb-0">Описание операции</p>
+          </div>
+          <small class="opacity-50 text-nowrap">$500.00</small>
+        </div>
+      </div>
+      <div class="list-group-item list-group-item-action py-3">
+        <div class="d-flex gap-2 w-100 justify-content-between">
+          <div>
+            <h6 class="mb-0">Списание с баланса</h6>
+            <p class="mb-2 opacity-75">2025-03-17 13:18:55</p>
+            <p class="mb-0">Описание операции</p>
+          </div>
+          <small class="opacity-50 text-nowrap">$100.45</small>
+        </div>
+      </div>
+      <div class="list-group-item list-group-item-action py-3">
+        <div class="d-flex gap-2 w-100 justify-content-between">
+          <div>
+            <h6 class="mb-0">Пополнение баланса</h6>
+            <p class="mb-2 opacity-75">2025-03-17 13:18:55</p>
+            <p class="mb-0">Описание операции</p>
+          </div>
+          <small class="opacity-50 text-nowrap">$500.00</small>
+        </div>
+      </div>
+      <div class="list-group-item list-group-item-action py-3">
+        <div class="d-flex gap-2 w-100 justify-content-between">
+          <div>
+            <h6 class="mb-0">Пополнение баланса</h6>
+            <p class="mb-2 opacity-75">2025-03-17 13:18:55</p>
+            <p class="mb-0">Описание операции</p>
+          </div>
+          <small class="opacity-50 text-nowrap">$500.00</small>
+        </div>
+      </div>
+    </div>
+    <div class="text-end">
+      <router-link :to="{name: 'transactions'}" class="btn btn-primary mt-3">
+        Подробнее
+      </router-link>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+main {
+  max-width: 460px;
+  margin: 4rem auto;
+}
+</style>
