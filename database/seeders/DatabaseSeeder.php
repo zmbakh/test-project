e6055e4a5042253e1554e4ebcd754fa1c89cf5cb
Zangar <zanggar.muratkhan@moravio.com>
2025-03-25 23:50:47 +0500

    Updating data on homepage

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,22 +2,40 @@
 
 namespace Database\Seeders;
 
+use App\Enums\Transaction\TransactionTypeEnum;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Services\Transaction\ProcessTransactionService;
+use App\Transfers\Transaction\ProcessTransactionTransfer;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Arr;
 
 class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
      */
-    public function run(): void
+    public function run(ProcessTransactionService $processTransactionService): void
     {
-        // User::factory(10)->create();
+        foreach (User::all() as $user) {
+            $processTransactionService->process(new ProcessTransactionTransfer(
+                $user->id,
+                TransactionTypeEnum::CREDIT,
+                50000,
+                fake()->sentence,
+            ));
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+            for ($i = 0; $i < 10; $i++) {
+                $processTransactionService->process(new ProcessTransactionTransfer(
+                    $user->id,
+                    Arr::random([
+                        TransactionTypeEnum::CREDIT,
+                        TransactionTypeEnum::DEBIT,
+                    ]),
+                    random_int(0, 1000),
+                    fake()->sentence,
+                ));
+            }
+        }
     }
 }
